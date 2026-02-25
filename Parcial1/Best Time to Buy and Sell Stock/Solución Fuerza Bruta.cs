cb1582b5edac393194285aab94170fdba255d750
Alzatiyo <alzatiyo10@gmail.com>
2026-02-25 14:46:02 -0500

    Best Time to Buy and Sell Stock

---
+++ Parcial1/Best Time to Buy and Sell Stock/Solución Fuerza Bruta.cs
@@ -0,0 +1,24 @@
+﻿/*
+    - Se realiza por fuerza bruta O(N^2), 
+    - Variable para la ganancia máxima.
+    - Recorremos el arreglo para tomar el día de compra.
+    - Para cada día de compra, recorremos los días siguientes como posibles días de venta.
+    - Si la ganancia (venta - compra) es mayor que la ganancia máxima actual, actualizamos la ganancia máxima.
+*/
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int GananciaMax = 0, T = prices.Length;
+
+        for (int i = 0; i < T - 1; i++)
+        {
+            for (int n = i + 1; n < T; n++)
+            {
+                if ((prices[n] - prices[i]) > GananciaMax)
+                    GananciaMax = prices[n] - prices[i];
+            }
+        }
+        return GananciaMax;
+    }
+}
---
+++ Parcial1/Best Time to Buy and Sell Stock/Solución Optimizada.cs
@@ -0,0 +1,21 @@
+﻿/*
+    - Se optimiza y se logra O(n) tiempo, O(1) espacio.
+    - Se recorre el vector una sola vez pero se mantiene el precio minimos, en cada
+        iteracion se calcula la posible ganancia si vendiera en el día actual y actualizo la ganancia máxima.
+*/
+using System;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int GananciaMax = 0, BuyPrice = int.MaxValue;
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            BuyPrice = Math.Min(BuyPrice, prices[i]);
+            GananciaMax = Math.Max(GananciaMax, prices[i] - BuyPrice);
+        }
+        return GananciaMax;
+    }
+}
